036693129b5fee82b00920370d19e6c337bb2dc6
jyasuu <s1002605@pu.edu.tw>
2025-05-01 14:02:30 +0000

    try peek

--- src/bin/bad_link_list.rs
+++ src/bin/bad_link_list.rs
@@ -38,6 +38,26 @@ impl List {
             }
         }
     }
+    
+    pub fn peek(&self) -> Option<&i32> {
+        match &self.head {
+            Link::Empty => None,
+            Link::More(node) => 
+            {
+                Some(&node.elem)
+            },
+        }
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut i32> {
+        match &mut self.head {
+            Link::Empty => None,
+            Link::More(node) => 
+            {
+                Some(&mut node.elem)
+            },
+        }
+    }
 }
 
 impl Drop for List {
@@ -81,6 +101,25 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        list.peek_mut().map(|value| {
+            *value = 42
+        });
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
 
