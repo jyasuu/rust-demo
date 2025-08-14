55bcacb45c0b8c1b94ebd02b076cb7d468a7b843
Jyasu <s1002605@pu.edu.tw>
2025-08-14 21:57:30 +0800

    Update condvar.rs

--- src/bin/condvar.rs
+++ src/bin/condvar.rs
@@ -24,3 +24,51 @@ fn main() {
     println!("Condition met, proceeding...");
     handle.join().unwrap();
 }
+
+
+#![allow(unused)]
+fn main_doc() {
+    use std::sync::{Arc, Mutex, Condvar};
+    use std::thread;
+    
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+    
+    println!("1");
+    // Inside of our lock, spawn a new thread, and then wait for it to start.
+    thread::spawn(move || {
+        println!("2");
+        thread::sleep(std::time::Duration::from_millis(100));
+        let (lock, cvar) = &*pair2;
+        let mut started = lock.lock().unwrap();
+        println!("3");
+        *started = true;
+        // We notify the condvar that the value has changed.
+        cvar.notify_one();
+        println!("4");
+    });
+    
+    println!("5");
+    // Wait for the thread to start up.
+    let (lock, cvar) = &*pair;
+    let mut started = lock.lock().unwrap();
+    println!("6");
+    while !*started {
+        println!("7");
+        started = cvar.wait(started).unwrap();
+        println!("8");
+    }
+    println!("9");
+}
+
+
+// 1
+// 5
+// 6
+// 7
+// 2
+// 3
+// 4
+// 8
+// 9
+
