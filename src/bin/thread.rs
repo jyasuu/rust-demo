df2f8385a8096b03533d064cddb0195c6cde452b
Jyasu <s1002605@pu.edu.tw>
2025-08-14 22:03:17 +0800

    Create thread.rs

---
+++ src/bin/thread.rs
@@ -0,0 +1,24 @@
+use std::thread;
+
+fn main() {
+    thread::spawn(f);
+    thread::spawn(f);
+    
+    thread::sleep(std::time::Duration::from_millis(100));
+
+    println!("Hello from the main thread.");
+}
+
+fn f() {
+    println!("Hello from another thread!");
+
+    let id = thread::current().id();
+    println!("This is my thread id: {id:?}");
+}
+
+
+// Hello from another thread!
+// This is my thread id: ThreadId(3)
+// Hello from another thread!
+// This is my thread id: ThreadId(2)
+// Hello from the main thread.
