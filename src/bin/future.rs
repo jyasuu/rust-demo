816d8d1fd634cbda4205df7456a223e4d931a906
Jyasu <s1002605@pu.edu.tw>
2026-01-12 21:30:03 +0800

    Implement dummy waker and manual Future example

--- src/bin/future.rs
+++ src/bin/future.rs
@@ -36,3 +36,52 @@ async fn main() {
     let result_block = my_async_block.await;
     println!("Async Block Result: {}", result_block);
 }
+
+
+
+
+
+
+// We need a "do-nothing" waker to satisfy the poll requirement
+fn dummy_waker() -> Waker {
+    fn raw_waker() -> RawWaker {
+        fn noop(_: *const ()) {}
+        fn clone(_: *const ()) -> RawWaker { raw_waker() }
+        let vtable = &RawWakerVTable::new(clone, noop, noop, noop);
+        RawWaker::new(std::ptr::null(), vtable)
+    }
+    unsafe { Waker::from_raw(raw_waker()) }
+}
+
+fn main() {
+    let waker = dummy_waker();
+    let mut cx = Context::from_waker(&waker);
+    
+    // 2. Using your manual Future
+    let manual_future = MyManualFuture {
+        message: "Hello from the manual Future!".to_string(),
+    };
+
+
+    // 3. Using your async block
+    let message = "Hello from the async block!".to_string();
+    let my_async_block = async move {
+        message
+    };
+    
+    
+    // We must "Pin" the future before polling it
+    let mut pinned_future = std::pin::pin!(manual_future);
+
+    // Manually trigger one poll
+    if let Poll::Ready(val) = pinned_future.as_mut().poll(&mut cx) {
+        println!("Manual Future Result {}", val);
+    }
+    // We must "Pin" the future before polling it
+    let mut pinned_future = std::pin::pin!(my_async_block);
+
+    // Manually trigger one poll
+    if let Poll::Ready(val) = pinned_future.as_mut().poll(&mut cx) {
+        println!("Async Block Result {}", val);
+    }
+}
